4b5e771be0a2ee2883f2f0563116a7665f80d889
Atharva-1512 <atharvagade403@gmail.com>
2026-03-27 21:44:32 +0530

    feat: add navbar component

---
+++ frontend/.history/src/App_20260327214420.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+  return (
+    <div>
+      <h1>DevPrep Hub 🚀</h1>
+    </div>
+  );
+}
+
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <h1>Welcome to DevPrep Hub 🚀</h1>
+    </div>
+  );
+}
+
+export default App;
